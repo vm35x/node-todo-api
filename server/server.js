4cc7327776ea52f22f8a2bc19a90517aee824fe8
Vladimir Mezentsov <vmezentsov@InComm.com>
2019-01-29 14:12:27 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
@@ -60,24 +61,56 @@ app.get("/todos/:id", (req, res) => {
 app.delete("/todos/:id", (req, res) => {
   //get id
   var id = req.params.id;
-  
+
   // validate id -> if not valid return 404
-  if(!ObjectID.isValid(id)){
-    return res.status(404).send()
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
 
   //DeprecationWarning: collection.findAndModify is deprecated. Use findOneAndUpdate, findOneAndReplace or findOneAndDelete instead.
   // remove todo by id
   Todo.findByIdAndRemove(id)
-  .then((todo) => {
-    if(!todo){
-      res.status(404).send()
-    }
-    res.status(200).send({todo})
-  }).catch(e => {
-    res.status(400).send()
-  })
-})
+    .then(todo => {
+      if (!todo) {
+        res.status(404).send();
+      }
+      res.status(200).send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
+app.patch("/todos/:id", (req, res) => {
+  //get id
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+
+  // validate id -> if not valid return 404
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  //Started up at port 3000
+// (node:19040) DeprecationWarning: collection.findAndModify is deprecated. Use findOneAndUpdate, findOneAndReplace or findOneAndDelete instead.
+  Todo.findOneAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if(!todo){
+        return res.status(404).send()
+      }
+      res.status(200).send({todo})
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
