49495331bb5d04b0e02e41fc73e5a8dc654a9767
Vladimir Mezentsov <vmezentsov@InComm.com>
2019-02-06 15:58:25 -0500

    clear

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -230,13 +230,15 @@ describe("GET users", () => {
           return done(err);
         }
 
-        User.findOne({ email }).then(user => {
-          expect(user).toBeTruthy();
-          // console.log('user.password ', user.password)
-          // console.log('password ', password)
-          expect(user.password).not.toBe(password) // TODO: figure out why failing
-          done();
-        }).catch((e) => done(e))
+        User.findOne({ email })
+          .then(user => {
+            expect(user).toBeTruthy();
+            // console.log('user.password ', user.password)
+            // console.log('password ', password)
+            expect(user.password).not.toBe(password); // TODO: figure out why failing
+            done();
+          })
+          .catch(e => done(e));
       });
   });
 
@@ -265,50 +267,75 @@ describe("GET users", () => {
 describe("POST /users/login", () => {
   it("should login user and return token", done => {
     request(app)
-    .post("/users/login")
-    .send({
-      email: users[1].email,
-      password: users[1].password,
-    })
-    .expect(200)
-    .expect(res => {
-      expect(res.headers["x-auth"]).toBeTruthy();
-    })
-    .end((err, res) => {
-      if(err){
-        done(err)
-      }
+      .post("/users/login")
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers["x-auth"]).toBeTruthy();
+      })
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
 
-      User.findById(users[1]._id).then((user) => {
-          expect(user.tokens[0]).toMatchObject({
-            access: 'auth',
-            token: res.headers['x-auth']
+        User.findById(users[1]._id)
+          .then(user => {
+            expect(user.tokens[0]).toMatchObject({
+              access: "auth",
+              token: res.headers["x-auth"]
+            });
+            done();
           })
-          done()
-      }).catch((e) => done(e))
-    })
+          .catch(e => done(e));
+      });
   });
 
   it("should reject invalid login", done => {
     request(app)
-    .post("/users/login")
-    .send({
-      email: users[1].email,
-      password: users[1].password + '1',
-    })
-    .expect(400)
-    .expect(res => {
-      expect(res.headers["x-auth"]).toBeFalsy();
-    })
-    .end((err, res) => {
-      if(err){
-        done(err)
-      }
+      .post("/users/login")
+      .send({
+        email: users[1].email,
+        password: users[1].password + "1"
+      })
+      .expect(400)
+      .expect(res => {
+        expect(res.headers["x-auth"]).toBeFalsy();
+      })
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
 
-      User.findById(users[1]._id).then((user) => {
-        expect(user.tokens.length).toBe(0)
-          done()
-      }).catch((e) => done(e))
-    })
+        User.findById(users[1]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+});
+
+describe("DELETE /users/me/token", () => {
+  it("should remove auth token on logout", done => {
+    request(app)
+      .delete("/users/me/token")
+      .set("x-auth", users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err){
+          return done(err)
+        }
+
+        User.findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
   });
 });
